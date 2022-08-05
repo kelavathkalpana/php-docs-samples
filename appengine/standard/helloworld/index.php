ff6fdaef40a39965811c6232c7465df2a9fde13b
kelavathkalpana <110654008+kelavathkalpana@users.noreply.github.com>
2022-08-04 22:35:21 -0700

    Update index.php

--- appengine/standard/helloworld/index.php
+++ appengine/standard/helloworld/index.php
@@ -1,3 +1,13 @@
 <?php
+echo "hello world!";
+echo "<a href='rform.html'>registration</a>";
+?>
 
-echo 'hello world!';
+rform.html
+**********
+<form method=post action=rverify.php>
+name <input name=a>
+<input type=submit>
+</form>
+
+
